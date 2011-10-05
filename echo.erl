68b537aa0bc261dc71d8a2fc17e42a8572a103b5
Kristján Pétursson <kristjan@gmail.com>
2011-10-05 02:03:32 -0700

    Enable hotswapping of a function sent over the net
    That's badass.
    
    %  > echo:netload(fun echo:run_louder/0, echoserver@<host>).
    %  > echo:shout("Hi", echoserver@<host>).

--- echo.erl
+++ echo.erl
@@ -1,9 +1,13 @@
 -module(echo).
 -export([
   echo/2,
+  shout/2,
   swap/1,
+  netload/2,
+
   start/0,
-  run/0
+  run/0,
+  run_louder/0
 ]).
 -define(GREETING, "You said ").
 
@@ -20,7 +24,19 @@ run() ->
     {swap, Sender} ->
       io:format("Received swap signal~n", []),
       Sender ! {reply, "Swapping in new code."},
-      ?MODULE:run()
+      ?MODULE:run();
+    {netload, Fn, Sender} ->
+      io:format("Received new code ~p~n", [Fn]),
+      Sender ! {reply, "Your wish is my command."},
+      Fn()
+  end.
+
+run_louder() ->
+  receive
+    {shout, Msg, Sender} ->
+      io:format("Received ~p~n", [Msg]),
+      Sender ! {reply, string:to_upper(Msg)},
+      run_louder()
   end.
 
 echo(Msg, Server) ->
@@ -30,8 +46,22 @@ echo(Msg, Server) ->
       io:format("~p~n", [Reply])
   end.
 
+shout(Msg, Server) ->
+  {echo, Server} ! {shout, Msg, self()},
+  receive
+    {reply, Reply} ->
+      io:format("~p~n", [Reply])
+  end.
+
 swap(Server) ->
   {echo, Server} ! {swap, self()},
+  receive
+    {reply, Reply} ->
+      io:format("~p~n", [Reply])
+  end.
+
+netload(Fn, Server) ->
+  {echo, Server} ! {netload, Fn, self()},
   receive
     {reply, Reply} ->
       io:format("~p~n", [Reply])
@@ -50,3 +80,5 @@ swap(Server) ->
 %  > c(echo).          % For echo:echo/2
 %  > echo:echo("Hi", echoserver@<host>).
 %  > echo:swap(echoserver@<host>).
+%  > echo:netload(fun echo:run_louder/0, echoserver@<host>).
+%  > echo:shout("Hi", echoserver@<host>).
